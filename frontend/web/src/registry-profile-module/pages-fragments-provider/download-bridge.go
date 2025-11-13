d3bae6aa304ab8cc26603cede8b597e5387fa40a
davimackulk <davimackulk@users.noreply.github.com>
2025-11-13 16:49:57 +0000

    Add boilerplate-report.java, download-bridge.go

---
+++ frontend/web/src/registry-profile-module/pages-fragments-provider/download-bridge.go
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/awardvote_fhe/frontend/web/src/registry-profile-module/pages-fragments-provider/download-bridge.go
+// Generated at: 2025-12-03T23:45:44.473Z
+
